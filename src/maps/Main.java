9000af9488b3c2ff85ddcc576a6e6d29c0735153
Dawid24 <paciorekdawidonly@gmail.com>
2018-08-05 23:07:57 +0200

    Initial commit

---
+++ src/maps/Main.java
@@ -0,0 +1,34 @@
+package maps;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("db.name", "cars");
+        map.put("db.user.name", "user1");// klucz, wartosc
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+
+        //cos tu robie
+
+        System.out.println(map.get("db.name"));
+
+        map.remove("db.name"); // tylko po kluczu
+        System.out.println(map.size());
+        Collection<String> wartosci = map.values();
+        System.out.println(wartosci);
+        int i = 0;
+        /*while (i < wartosci.size()) {
+            // cos tu robimy
+            i++;
+        }*/
+
+        for (String value : wartosci) {
+            System.out.println("Test: " + value);
+        }
+
+    }
+}
---
+++ src/sets/Main.java
@@ -0,0 +1,17 @@
+package sets;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> set = new TreeSet<>();
+        set.add(1);
+        set.add(5);
+        set.add(3);
+
+        for (Integer integer : set) {
+            System.out.println(integer); //sortuje
+        }
+    }
+}
